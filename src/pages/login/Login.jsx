5174d9e5f11f69507a284aaf9cf255d21b51a69c
Sanja Daic <sanja.daic86@gmail.com>
2026-01-09 08:57:40 +0100

    Creating Login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(-1); //to go one back to the page
+    localStorage.setItem("user", JSON.stringify(user));
+    setUser("");
+    setEmail("");
+  };
+
+  return (
+    <div className="loginWrapper">
+      <div className="formWrapper">
+        <form onSubmit={handleSubmit}>
+          <div className="inputWrapper">
+            <label htmlFor="name">Name</label>
+            <input
+              onChange={(event) => setUser(event.target.value)}
+              type="text"
+              id="name"
+              value={user}
+            />
+          </div>
+          <div className="inputWrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              id="name"
+              value={email}
+            />
+          </div>
+
+          <button type="submt">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
