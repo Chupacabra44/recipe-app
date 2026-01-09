dfab9ce3b4b1867a39b74e54577381d4786e12d9
Sanja Daic <sanja.daic86@gmail.com>
2026-01-09 12:05:47 +0100

    Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
+  // const [query, setQuery] = useState("");
+  const [selectedMeal, setSelectedMeal] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  // const appId = process.env.REACT_APP_API_ID;
+  // const appKey = process.env.REACT_APP_API_KEY;
+
+  //https://developer.edamam.com/edamam-docs-recipe-api - doesn't work like before 2 years
+
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${selectedMeal}`;
+
+  const getData = async () => {
+    try {
+      const { data } = await axios(url);
+      setRecipes(data.meals);
+      console.log(data.meals);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(recipes);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div>Home</div>;
 };
 
