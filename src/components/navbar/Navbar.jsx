b5653170899f299d7d487a606bbb4576ba609b0e
Sanja Daic <sanja.daic86@gmail.com>
2026-01-09 09:55:39 +0100

    Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,34 @@
+import { NavLink, Link } from "react-router-dom";
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow(!show);
+  };
+
+  return (
+    <nav className="navWrapper">
+      <div>
+        <Link to="/">
+          <i>{"<OAK/>"}</i>
+          <span>Recipe</span>
+        </Link>
+      </div>
+
+      <div className={show ? "linkWrapper show" : "linkWrapper"}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+        <NavLink to="login">Login</NavLink>
+      </div>
+
+      <div onClick={toggleMenu} className="burgerMenu">
+        &#9776;
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
